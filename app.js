8192480cfb4cae8c9639961b06095281fb0a7560
Sandeep-Petwal <sandeeppetwal51@gmail.com>
2025-03-13 22:13:33 +0530

    re-arrange repo

--- app.js
+++ app.js
@@ -4,16 +4,24 @@ const routes = require('./routes/index.js')
 const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler.js')
 const cookieParser = require('cookie-parser');
-const { globalLimiter    } = require("./utils/limiter.js")
+const { globalLimiter } = require("./utils/limiter.js")
 
 // middleware
 const app = express();
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000', // specify the exact origin
+    credentials: true, // allow credentials (cookies, authorization headers)
+  };
+  
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
 // routes
-app.use("/api/v1", globalLimiter, routes);
+app.use("/api/v1", (req, res, next) => {
+    console.log("Route :: ", req.path);
+    next();
+}, globalLimiter, routes);
 
 // error handling middleware
 app.use(errorHandler);
--- utils/limiter.js
+++ utils/limiter.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit');
 
 // Define a global limiter for all routes
 const globalLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // 15 minutes
+    windowMs: 10000 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
     message: 'Too many requests, please try again later.',
     standardHeaders: true,
@@ -13,7 +13,7 @@ const globalLimiter = rateLimit({
 
 // Define a limiter for sensitive routes (e.g., login, password reset)
 const authLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // 60 minute
+    windowMs: 10000 * 60 * 1000, // 60 minute
     max: 20, // limit each IP to 5 requests per minute
     message: 'Too many requests, please try again later.',
     standardHeaders: true,
@@ -22,7 +22,7 @@ const authLimiter = rateLimit({
 
 // Define a limiter for API routes
 const freeUrlLimiter = rateLimit({
-    windowMs: 24 * 60 * 60 * 1000, // 1 day
+    windowMs: 24000 * 60 * 60 * 1000, // 1 day
     max: 20, // limit each IP to 20 requests per minute
     message: 'Too many requests, please try again later.',
     standardHeaders: true,
