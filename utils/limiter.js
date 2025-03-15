83b3d05200898b8870689fa4e18cd4480a8cb3c7
Sandeep-Petwal <sandeeppetwal51@gmail.com>
2025-03-15 13:24:07 +0530

    project complete

--- utils/limiter.js
+++ utils/limiter.js
@@ -5,7 +5,7 @@ const rateLimit = require('express-rate-limit');
 // Define a global limiter for all routes
 const globalLimiter = rateLimit({
     windowMs: 10000 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 1000, // limit each IP to 100 requests per windowMs
     message: 'Too many requests, please try again later.',
     standardHeaders: true,
     legacyHeaders: false,
@@ -14,7 +14,7 @@ const globalLimiter = rateLimit({
 // Define a limiter for sensitive routes (e.g., login, password reset)
 const authLimiter = rateLimit({
     windowMs: 10000 * 60 * 1000, // 60 minute
-    max: 20, // limit each IP to 5 requests per minute
+    max: 200, // limit each IP to 5 requests per minute
     message: 'Too many requests, please try again later.',
     standardHeaders: true,
     legacyHeaders: false,
@@ -23,7 +23,7 @@ const authLimiter = rateLimit({
 // Define a limiter for API routes
 const freeUrlLimiter = rateLimit({
     windowMs: 24000 * 60 * 60 * 1000, // 1 day
-    max: 20, // limit each IP to 20 requests per minute
+    max: 200, // limit each IP to 20 requests per minute
     message: 'Too many requests, please try again later.',
     standardHeaders: true,
     legacyHeaders: false,
